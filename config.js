78122a22ec74d7709269213f3f06cd1129b5786d
longvp <phamvietlong.pass@gmail.com>
2022-05-25 11:20:38 +0700

    commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Nga Heo';
+const giftUrl = 'http://facebook.com';
+const eventName = 'Happy BirthDay';
+const titleCard = 'Tặng Nga';
+const contentCard = 'Chúc Nga luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Nga trong hôm nay và cả những ngày sau ...';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
